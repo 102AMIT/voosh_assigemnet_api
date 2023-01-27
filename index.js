835ea3ceb591a4c3645c8b527604f93c6b00c1f3
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-27 18:02:34 +0530

    basic server setup completed

---
+++ index.js
@@ -0,0 +1,19 @@
+const express =require('express');
+const app = express();
+const port = 8000;
+
+
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+
+
+app.listen(port,function(error){
+    if(error){
+        console.log(error);
+    }
+    console.log(`App running on http://localhost:${port}`);
+});
