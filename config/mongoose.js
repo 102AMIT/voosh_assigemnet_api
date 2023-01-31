cb7923ee722748f7c070c2d675dfd95668a1bdcb
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-31 22:17:21 +0530

    db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 mongoose.set("strictQuery", false);
-mongoose.connect(`${process.env.DB_URL}`);
+mongoose.connect("mongodb+srv://voosh:voosh@cluster0.q369kps.mongodb.net/test");
 const db=mongoose.connection;
 
 db.on('error',console.error.bind(console,"Error is conecting to MongoDB"));
